7dcb36f34d5c95c2ab56b69b42cde2106b59e5ab
cllcnkrt <cllcnkrt@outlook.com>
2021-08-17 21:42:30 +0300

    project done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Result from "./components/Result/Result";
+import requests from "./requests";
 
 function App() {
+  
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending)
+
   return (
     <div className="app">
       <Header />
 
-      <Nav />
-      <Result/>
+      <Nav setSelectedOption={setSelectedOption}/>
+      <Result selectedOption={selectedOption}/>
     </div>
   );
 }
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import './Result.css'
+import axios from "../../axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import VideoCard from "../VideoCard/VideoCard";
+import "./Result.css";
+import FlipMove from "react-flip-move";
 
-function Result() {
-    return (
-        <div className="result">
-            <VideoCard/>
-            <VideoCard/>
-            <VideoCard/>
-            <VideoCard/>
-            <VideoCard/>
-        </div>
-    )
+
+function Results({ selectedOption }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(selectedOption);
+      setMovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [selectedOption]);
+
+  return (
+    <div className="result">
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
+    </div>
+  );
 }
 
-export default Result
+export default Results;
