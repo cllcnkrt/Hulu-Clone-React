3c424e4d60bb50cb978b87cdea2ef373843243f3
cllcnkrt <cllcnkrt@outlook.com>
2021-08-17 14:28:11 +0300

    header component done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hulu</h1>
+    <div className="app">
+
+      <Header/>
+      
+      {/* Nav */}
+      {/* Result */}
     </div>
   );
 }
